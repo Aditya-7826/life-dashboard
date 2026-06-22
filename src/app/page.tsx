6a8cd492675dd71ba8f700be85efb3cccfb6db9c
Aditya <adityaverma7826@gmail.com>
2026-06-22 15:26:57 +0530

    Redirect homepage to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main className="p-10 text-white bg-black min-h-screen">
-      <h1>Environment Test</h1>
-
-      <p>
-        URL:
-        {process.env.NEXT_PUBLIC_SUPABASE_URL
-          ? " FOUND"
-          : " MISSING"}
-      </p>
-
-      <p>
-        KEY:
-        {process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-          ? " FOUND"
-          : " MISSING"}
-      </p>
-    </main>
-  );
+  redirect("/dashboard");
 }
